e24f74c00716c076f3a25dbb4b12aeeedb4854b7
C4illin <c@illin.cf>
2020-03-17 17:55:46 +0100

    Added !klass command

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const botToken = config.discord
 // Invite: https://discordapp.com/oauth2/authorize?client_id=689153258648109151&scope=bot&permissions=268445696
 
 bot.on("ready", () => {
-  bot.user.setActivity('!elev för att bli elev', { type: 'LISTENING' })
+  bot.user.setActivity('!elev', { type: 'LISTENING' })
   console.log("Discord.js Connected")
 })
+
+const klasser = ["1a", "1b", "1c", "1d", "1e", "2A", "2B", "2C", "2D", "2E", "3a", "3b", "3c", "3d", "3e"]
 
 bot.on("message", (message) => {
   if (message.author.bot) return
@@ -22,9 +24,50 @@ bot.on("message", (message) => {
   case "help":
     message.channel.send({
       embed: new Discord.MessageEmbed()
-        .addField("Allmäna kommandon:", "!help - visar denna meny\n!elev - bli elev")
+        .addField("Allmäna kommandon:", "!help - visar denna meny\n!elev - bli elev\n!klass [klass] - gå med i en klass")
         .setColor("0x111111")
     })
+    break
+  case "klass":
+    var klass = split[1]
+    message.delete()
+    if (klasser.includes(klass) || klasser.includes(klass.toLowerCase()) || klasser.includes(klass.toUpperCase())){
+      if (klasser.includes(klass.toLowerCase())) {
+        klass = klass.toLowerCase()
+      } else if (klasser.includes(klass.toUpperCase())) {
+        klass = klass.toUpperCase()
+      }
+
+      if (message.member.roles.cache.some(role => role.name !== 'Elev')) {
+        const elev = message.guild.roles.cache.find(role => role.name === 'Elev')
+        message.member.roles.add(elev)
+      }
+
+      if (message.member.roles.cache.some(role => klasser.includes(role.name))) {
+        message.reply("du är redan med i en klass")
+          .then(msg => {
+            msg.delete({timeout:5000, reason:"Redan klasskamrat"})
+          })
+      } else {
+        const klassRoll = message.guild.roles.cache.find(role => role.name === klass)
+        message.member.roles.add(klassRoll)
+          .then(message.reply(`du är nu med i klass ${klass}`).then(msg => {
+            msg.delete({timeout:5000, reason:"Gick med i klass " + klass})
+          }))
+          .catch(err => {
+            console.log(err)
+            message.reply(`Fel inträffande vid givande av roll ${klass}, avbryter.`)
+              .then(msg => {
+                msg.delete({timeout:5000, reason:"Kunde inte ge roll"})
+              })
+          })
+      }
+    } else {
+      message.reply(klass+" är ingen giltig klass")
+        .then(msg => {
+          msg.delete({timeout:5000, reason:"Ej giltig klass"})
+        })
+    }
     break
   case "e":
   case "elev":
